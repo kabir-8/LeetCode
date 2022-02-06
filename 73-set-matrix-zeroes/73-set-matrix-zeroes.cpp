8156842c2c1f4df0a125f963c3aea07ddd408384
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-06 15:33:05 +0530

    Time: 27 ms (25.28%), Space: 13.2 MB (82.00%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-         int m = matrix.size();
-        int n = matrix[0].size();
-        //vector row stores the ros which contain 0
-        //similarly vector col stores column which have 0
-        vector<int> row;
-        vector<int> col;
-        //here we start traversing the 2d matrix 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                //if a 0 element is enciuntered then its row and column values are stored in row and column vectors respectively
-                if(matrix[i][j] == 0){
-                    
-                    row.push_back(i);
-                    col.push_back(j);
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][j]=93164;
                 }
             }
         }
-        //now our row and column vectors contain rows and column that have 0
-        //we traverse row vector find which row has a zero and change all its elements to 0
-        for(int i = 0 ; i < row.size() ; i++){
-            for(int j = 0 ; j < n ; j++){
-                matrix[row[i]][j] = 0;
-            }
-        }
-        //we traverse col vector find which column has a zero and change all its elements to 0
-        for(int i = 0 ; i < col.size() ; i++){
-            for(int j = 0 ; j < m ; j++){
-                matrix[j][col[i]] = 0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==93164)
+                {
+                    for(int a=0;a<row;a++)
+                    {
+                        if(matrix[a][j]!=93164)
+                            matrix[a][j]=0;
+                    }
+                    for(int b=0;b<col;b++)
+                    {
+                        if(matrix[i][b]!=93164)
+                            matrix[i][b]=0;
+                    }
+                    matrix[i][j]=0;
+                }
             }
         }
     }
-    
 };
