b0cf9fed80bb34e21149ddb52a1f2fad3095be31
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-12 08:01:16 +0530

    Time: 181 ms (55.00%), Space: 43.4 MB (27.87%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.c
+++ 234-palindrome-linked-list/234-palindrome-linked-list.c
@@ -9,7 +9,7 @@
 bool palindrome(int arr[], int n)
 {
     int flag = 0;
-    for (int i = 0; i <= n / 2 ; i++) {
+    for (int i = 0; i <= n / 2 && n != 0; i++) {
         if (arr[i] != arr[n - i - 1]) {
             flag = 1;
             break;
