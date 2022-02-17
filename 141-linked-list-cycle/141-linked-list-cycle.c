bb88d78474e32b77f8574fb79589ac01f38e4a2b
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-17 14:32:32 +0530

    Time: 12 ms (55.59%), Space: 7.7 MB (99.63%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if(head==NULL)
+        return false;
+    struct ListNode *temp=head;
+    struct ListNode *prev;
+    while(temp->next!=NULL)
+    {
+        prev=temp;
+        temp=temp->next;
+        prev->val=-9;
+        prev->next=NULL;
+    }
+    if(temp->val==-9)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
