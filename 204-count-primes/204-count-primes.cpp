02e8eb80ba962609da23affc206279791263ddca
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-18 16:19:42 +0530

    Time: 203 ms (69.90%), Space: 10.3 MB (70.31%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool>arr(n,true);
+        vector<bool> isPrime(n, true);
         long long int count=0;
-        if(n==0 ||n==1||n==2)
+        if(n==0||n==1||n==2)
             return 0;
-        for(long long int i=2;i<n;i++)
-        {
-            if(arr[i])
+        for(long long int i=2;i<n;i++){
+            if(isPrime[i]){
                 count++;
-            for(long long int j=i*i;j<n;j+=i)
-            {
-                arr[j]=false;
+                for(long long int j=i*i;j<n;j=j+i){
+                    isPrime[j]=false;
+                }
             }
         }
         return count;
