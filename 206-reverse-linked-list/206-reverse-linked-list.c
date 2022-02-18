677aea8039668ec11ec546ef55d4115207c22ed8
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-18 12:17:06 +0530

    Time: 6 ms (31.60%), Space: 6.4 MB (63.28%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.c
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -8,24 +8,16 @@
 
 
 struct ListNode* reverseList(struct ListNode* head){
-    struct ListNode *temp=head;
-    int arr[5000];
-    int i=0;
-    while(temp!=NULL)
+    struct ListNode *curr=head;
+    struct ListNode *next=NULL;
+    struct ListNode *prev=NULL;
+    while(curr!=NULL)
     {
-        int x=temp->val;
-        arr[i]=x;
-        temp=temp->next;
-        i++;
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
     }
-    struct ListNode *temp2=head;
-    i=i-1;
-    while(temp2!=NULL)
-    {
-        temp2->val=arr[i];
-        temp2=temp2->next;
-        i--;
-    }
+    head=prev;
     return head;
-    
 }
