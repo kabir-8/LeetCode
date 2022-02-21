c90f752d31d61d01c941fe7051805c7f4e478ca6
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-02-21 15:28:59 +0530

    Time: 10 ms (63.96%), Space: 7.7 MB (90.51%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.c
+++ 141-linked-list-cycle/141-linked-list-cycle.c
@@ -1,10 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 bool hasCycle(struct ListNode *head) {
     if(head==NULL)
         return false;
