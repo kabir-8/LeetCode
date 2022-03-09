4b57773547a2b73e3c4822f06af0314a86dc4ee7
KaranBir Singh <56889117+kabir-8@users.noreply.github.com>
2022-03-09 10:55:51 +0530

    Time: 285 ms (18.60%), Space: 85.3 MB (9.65%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,50 +1,34 @@
 class Solution
 {
 public:
-// next smaller array
-    void nextSmaller(vector<int> &arr, vector<int> &next)
-    {
-        stack<int> st;
-        for (int i = arr.size() - 1; i >= 0; i--)
-        {
-            while (!st.empty() and arr[st.top()] >= arr[i])
-            {
-                st.pop();
+int largestRectangleArea(vector<int>& heights) {
+        int len = heights.size();
+        vector<int> right(len, len);
+        stack<int> monoIncStackRight;
+        for (int i = 0; i < len; i++) {
+            while (!monoIncStackRight.empty() && heights[i] < heights[monoIncStackRight.top()]) {
+                right[monoIncStackRight.top()] = i;
+                monoIncStackRight.pop();
             }
-            int n = st.empty() ? arr.size() - 1 : st.top() - 1;
-            next[i] = n;
-            st.push(i);
+            monoIncStackRight.push(i);
         }
-    }
-    // previous smaller array
-    void prevSmaller(vector<int> &arr, vector<int> &prev)
-    {
-        stack<int> st;
-        for (int i = 0; i < arr.size(); i++)
-        {
-            while (!st.empty() and arr[st.top()] >= arr[i])
-            {
-                st.pop();
+        
+        vector<int> left(len, -1);
+        stack<int> monoIncStackLeft;
+        for (int i = len - 1; i >= 0; i--) {
+            while (!monoIncStackLeft.empty() && heights[i] < heights[monoIncStackLeft.top()]) {
+                left[monoIncStackLeft.top()] = i;
+                monoIncStackLeft.pop();
             }
-            int p = st.empty() ? 0 : st.top() + 1;
-            prev.push_back(p);
-            st.push(i);
+            monoIncStackLeft.push(i);
         }
-    }
-    int largestRectangleArea(vector<int> &heights)
-    {
-        // next array
-        vector<int> next(heights.size(), -1);
-        // previous array
-        vector<int> prev;
-        prevSmaller(heights, prev);
-        nextSmaller(heights, next);
-        int res = 0, area;
-        for (int i = 0; i < heights.size(); i++)
-        {
-            area = (next[i] - prev[i] + 1) * heights[i];
-            res = max(res, area);
+        
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            int area = (right[i] - left[i] - 1) * heights[i];
+            result = max(area, result);
         }
-        return res;
+        
+        return result;
     }
 };
